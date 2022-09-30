752b9e4beba68ab67f9911b176c4683c0264cb4b
adil080891 <97826407+adil080891@users.noreply.github.com>
2022-09-30 15:28:59 +0500

    UI updation
    UI updation

--- src/screens/authFlow/confirmPhone/index.js
+++ src/screens/authFlow/confirmPhone/index.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react'
-import { View, SafeAreaView, StatusBar, Text, Image, StyleSheet } from 'react-native'
+import { View, SafeAreaView, StatusBar, Text, Image, } from 'react-native'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
 import { Input } from '../../../components/textInput';
-import { appImages, colors, fontFamily, hp, routes, wp } from '../../../services'
+import { appImages, colors, routes, } from '../../../services'
 import appStyles from '../../../services/utilities/appStyles'
 import Button from '../../../components/button';
 import { styles } from './styles';
@@ -16,20 +16,22 @@ const ConfirmPhone = (props) => {
             <KeyboardAwareScrollView
                 keyboardShouldPersistTaps="always"
                 contentContainerStyle={appStyles.scrollContainer}>
-                <View style={styles.bgIMageView}>
-                    <Image resizeMode='contain' style={styles.imageStyle} source={appImages.confirmPhoneIcon} />
-                </View>
-                <View style={styles.titleView}>
-                    <Text style={styles.loginText}>Confirm Phone</Text>
-                </View>
-                <View style={styles.textInputView}>
-                    <Input placeholder={"Phone Number"} keyboardType={"number-pad"} onChangeText={(text) => setIsText(text)} value={isText} >Phone Number</Input>
-                </View>
-                <View style={styles.buttonView}>
-                    <Button onPress={() => props.navigation.navigate(routes.verification)} containerStyle={styles.buttonStyle} style={{ fontSize: 13 }} >Send OTP</Button>
-                </View>
-                <View style={styles.noteTextView}>
-                    <Text style={styles.noteText}>Note: Please enter a valid phone number where you can receive OTP</Text>
+                <View style={[appStyles.flex1]}>
+                    <View style={styles.bgIMageView}>
+                        <Image resizeMode='contain' style={styles.imageStyle} source={appImages.confirmPhoneIcon} />
+                    </View>
+                    <View style={styles.titleView}>
+                        <Text style={styles.loginText}>Confirm Phone</Text>
+                    </View>
+                    <View style={styles.textInputView}>
+                        <Input placeholder={"Phone Number"} keyboardType={"number-pad"} onChangeText={(text) => setIsText(text)} value={isText} >Phone Number</Input>
+                    </View>
+                    <View style={styles.buttonView}>
+                        <Button onPress={() => props.navigation.navigate(routes.verification)} containerStyle={styles.buttonStyle} style={{ fontSize: 13 }} >Send OTP</Button>
+                    </View>
+                    <View style={styles.noteTextView}>
+                        <Text style={styles.noteText}>Note: Please enter a valid phone number where you can receive OTP</Text>
+                    </View>
                 </View>
             </KeyboardAwareScrollView>
         </SafeAreaView>
